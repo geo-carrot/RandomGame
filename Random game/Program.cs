cc735932a6aeb591fa3dd65ba41548c765c03ce8
Дмитрий <@DESKTOP-HVG9QKI>
2022-09-15 14:38:52 +0400

    Добавьте файлы проекта.

---
+++ Random game/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Random x = new Random();
+
+int pn = x.Next(0, 51);
+
+int hn = 0;
+int count = 0;
+
+Console.WriteLine("Программой загадано число от 0 до 50.");
+Console.WriteLine("Угадайте это число. Количество попыток не ограничено");
+
+while (hn != pn)
+{
+    hn = Convert.ToInt32(Console.ReadLine());
+
+    {
+        Console.WriteLine("Вы не угадали число.");
+    }
+    count++;
+    if (hn == pn)
+    {
+        Console.WriteLine("Поздравляем, Вы угадали число с попытки № " + count + ":");
+    }
+}
